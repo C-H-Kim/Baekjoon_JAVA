1806a2fca7c218ab4727c3b4fd3f0e301a816864
Chang hun <nick6253@naver.com>
2022-07-29 18:29:49 +0900

    I/O & Arithmetic Operation

---
+++ src/bj_1001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class bj_1001 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.printf("%d", a-b);
+    }
+}
