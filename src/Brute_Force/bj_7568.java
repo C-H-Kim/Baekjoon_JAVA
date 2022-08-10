54c533cee1c5c59b151759499f499f28051aadcc
C-H-Kim <nick6253@naver.com>
2022-08-11 05:32:37 +0900

    Brute Force

--- src/Brute_Force/bj_7568.java
+++ src/Brute_Force/bj_7568.java
@@ -1,4 +1,40 @@
 package Brute_Force;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class bj_7568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] weight = new int[N];
+        int[] height = new int[N];
+        int[] cnt = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            weight[i] = Integer.parseInt(st.nextToken());
+            height[i] = Integer.parseInt(st.nextToken());
+            cnt[i] = 1;
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(j == i) {
+                    continue;
+                }
+
+                if(weight[i] < weight[j] && height[i] < height[j]) {
+                    cnt[i]++;
+                }
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            System.out.print(cnt[i] + " ");
+        }
+    }
 }
