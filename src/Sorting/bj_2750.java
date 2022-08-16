1f55b7130e512507d1ae81214ac32df0db8b90cf
Chang hun <nick6253@naver.com>
2022-08-16 18:38:14 +0900

    Sorting

---
+++ src/Sorting/bj_2750.java
@@ -0,0 +1,35 @@
+package Sorting;
+
+import java.io.*;
+
+public class bj_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        insertion_sort(arr);
+
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void insertion_sort(int[] arr) {
+        for(int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
