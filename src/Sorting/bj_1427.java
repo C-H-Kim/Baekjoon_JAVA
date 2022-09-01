a70ce86296a31f96b2a84edf40b32ff71b43799f
Chang hun <nick6253@naver.com>
2022-09-01 16:46:42 +0900

    Sorting

--- src/Sorting/bj_1427.java
+++ src/Sorting/bj_1427.java
@@ -3,9 +3,32 @@ package Sorting;
 import java.io.*;
 
 public class bj_1427 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String N = br.readLine();
+        int[] arr = new int[N.length()];
+
+        for(int i = 0; i < N.length(); i++) {
+            arr[i] = N.charAt(i) - 48;
+        }
+
+        insertion_Sort(arr);
+
+        for(int k : arr) {
+            System.out.print(k);
+        }
     }
 
-
+    public static void insertion_Sort(int[] arr) {
+        for(int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] < key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
